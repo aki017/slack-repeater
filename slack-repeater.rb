0a48f6d865fcbc4d2c12c6c71c7f3852f026fa31
Tomohiro Nishimura <tomohiro68@gmail.com>
2014-02-20 17:22:40 +0900

    Attachment を使ってみやすくしました。

--- slack-repeater.rb
+++ slack-repeater.rb
@@ -3,17 +3,18 @@ require 'faraday'
 require 'sinatra/base'
 require 'json'
 
-def post(name, message, channel)
+def post(name, message, channel, options = {})
   connection = Faraday.new(url: 'https://slack.com/') do |faraday|
     faraday.request  :url_encoded
     faraday.adapter  :net_http
   end
-  connection.post '/api/chat.postMessage', {
+  data = {
     token: ENV['SLACK_TOKEN'],
     channel: channel,
     text: message,
-    username: name
-  }
+    username: name,
+  }.merge(options)
+  connection.post '/api/chat.postMessage', data
 end
 
 class SlackRepeater < Sinatra::Base
@@ -26,12 +27,36 @@ class SlackRepeater < Sinatra::Base
     project = error['project']
     environment = error['environment']
 
-    message = "<https://happyelements.airbrake.io/projects/#{project['id']}/groups/#{error['id']}|#{error['error_message']}>\n"
-    message << "　- peoject name: #{project['name']}\n"
-    message << "　- environment: #{environment}\n"
-    message << "　- last occurence: #{error['last_occurred_at']}\n"
-    message << "　- times occurred: #{error['times_occurred']}\n"
-    post "Airbrake [#{environment}]", message, ENV["SLACK_AIRBRAKE_CHANNEL_#{project['id']}"]
+    message = "<https://happyelements.airbrake.io/projects/#{project['id']}/groups/#{error['id']}|#{error['error_message']}>"
+
+    text = error['error_message']
+    fields = [
+      ["project name", project["name"], true],
+      ["environment", environment, true],
+      ["times occurred", error["times_occurred"], true],
+      ["last occurence", error["last_occurred_at", true]],
+      ["request url", error["request_url", false]],
+    ]
+
+    attachments = [{
+      fallback: text,
+      color: "#ff0000",
+      fields: fields.map {|title, value, short|
+        {
+          title: title,
+          value: value,
+          short: short,
+        }
+      }
+    }]
+
+    options = {
+      attachments: attachments.to_json,
+      icon_emoji: ":scream:",
+    }
+
+    post "Airbrake [#{environment}]", message, ENV["SLACK_AIRBRAKE_CHANNEL_#{project['id']}"], options
+
   end
 end
 
