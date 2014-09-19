58eb0a9fc217f5f0d4282e5a3767c3758e27e8d1
Tomohiro Nishimura <tomohiro68@gmail.com>
2014-09-19 15:17:17 +0900

    多分 true じゃなくて 1 を指定する必要がある

--- slack-repeater.rb
+++ slack-repeater.rb
@@ -56,7 +56,7 @@ class SlackRepeater < Sinatra::Base
     options = {
       attachments: attachments.to_json,
       icon_emoji: ":scream:",
-      unfurl_links: true,
+      unfurl_links: 1,
     }
 
     post "Airbrake [#{environment}]", message, ENV["SLACK_AIRBRAKE_CHANNEL_#{project['id']}"], options
